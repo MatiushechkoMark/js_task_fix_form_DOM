b294f9a65a2860c4c89a50db424dca500aa95a0e
MatiushechkoMark <mmatiushechko@gmail.com>
2024-06-11 03:28:12 +0300

    make build

---
+++ index.f75de5e1.js
@@ -0,0 +1,18 @@
+"use strict";
+function camelCaseToText(str) {
+    return str.replace(/([a-z])([A-Z])/g, "$1 $2").replace(/^\w/, (c)=>c.toUpperCase());
+}
+const forms = document.querySelectorAll("form");
+forms.forEach((form)=>{
+    const inputs = form.querySelectorAll("input");
+    inputs.forEach((input)=>{
+        const label = document.createElement("label");
+        label.classList.add("field-label");
+        label.setAttribute("for", input.id);
+        label.textContent = camelCaseToText(input.name);
+        input.setAttribute("placeholder", camelCaseToText(input.name));
+        input.parentNode.insertBefore(label, input);
+    });
+});
+
+//# sourceMappingURL=index.f75de5e1.js.map
